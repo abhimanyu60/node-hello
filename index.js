0807f97fcabab798c26bbae5e9a664a91ca1513b
abhimanyu60 <87974964+abhimanyu60@users.noreply.github.com>
2025-01-16 15:02:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
-});
+app.get('/', (req, res) => {
+        res.send('hello world');
+        res.end();
+}
+)
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
-});
+app.listen(8081, () => {
+console.log('server is running')
+})
